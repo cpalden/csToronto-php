4c3a88f0646b26fd82a2ef28c81c7beb7f308f03
cpalden <cpalden@scs.ryerson.ca>
2018-05-07 17:00:29 -0400

    update

--- report.php
+++ report.php
@@ -1,1 +1,14 @@
-<?php session_start();?>
+<?php
+include '/includes/connect406.php';
+
+$prob = $_POST['problem'];
+$loc = $_POST['loc'];
+$p=implode(", ",$prob);
+
+$sql = "INSERT INTO problems (problems, location) VALUES ('$p', '$loc')";
+$result = $conn->query($sql);
+if($result){
+	header("Location: /cs/index.html");
+	}
+	
+?>
